673e96f398b713b77f24466f25af67eb04d6897e
aamir <mohammad.aamir@yahoo.com>
2017-02-23 15:10:16 -0800

    More Functionality

---
+++ server/common/src/main/java/com/abc/xyz/common/data/Comment.java
@@ -0,0 +1,50 @@
+package com.abc.xyz.common.data;
+
+public class Comment {
+
+    private String id;
+    private String user;
+    private long time;
+    private String comment;
+    private String blogId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(String blogId) {
+        this.blogId = blogId;
+    }
+}
